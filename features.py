fc77f36ae8a923a0365622e5082405f467ee54a9
Jefferson Lee <jeffersonlee@college.harvard.edu>
2015-12-01 17:04:40 -0500

    Added some comments

--- features.py
+++ features.py
@@ -16,5 +16,8 @@ def kernel(mu, sigma, x1, x2):
   C = norm.cdf(1 - mu, sigma) - norm.cdf(-1 * mu, sigma)
   return (norm.cdf(x2, mu, sigma) - norm.cdf(x1, mu, sigma)) / C
 
+# tweet is the pure text of the tweet
+# vocab is a dictionary from the lowercase words to their ids
+# sigma is a parameter for lowbow. We may want to generalize this.
 def get_features(tweet, vocab, sigma):
   return lowbow(tweet, vocab, sigma)
