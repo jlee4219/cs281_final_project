2968ae44e76b46b2448bd092c1ebbf9c9f21ef68
Jefferson Lee <jeffersonlee@college.harvard.edu>
2015-12-03 15:48:33 -0500

    started feature selection

--- feature_selection.py
+++ feature_selection.py
@@ -17,10 +17,23 @@ def df(data, cutoffs, num_feats):
 # Darmstadt Indexing Approach. Same input as above except for authors
 # authors is an array of size tweets providing the corresponding author for each tweet in data
 def dia(data, authors, cutoffs, num_feats):
-  dia = np.zeros(data.shape[1], len(set(authors)))
+  vals = np.zeros(data.shape[1], len(set(authors)))
   for feature in range(data.shape[1]):
     for author in set(authors):
-      a = len(np.where(data[:, np.where(authors == author)] > cutoffs[feature])[0])
-      b = len(np.where(data[:, np.where(authors != author)] > cutoffs[feature])[0])
-      dia[feature, author] = float(a) / (a + b)
-  dia = np.amax(dia, axis=1)
+      a = len(np.where(data[np.where(authors == author), feature] > cutoffs[feature])[0])
+      b = len(np.where(data[np.where(authors != author), feature] > cutoffs[feature])[0])
+      vals[feature, author] = float(a) / (a + b)
+  vals = np.amax(vals, axis=1)
+  return np.argpartition(vals, -num_feats)[-num_feats:]
+
+def odds_ratio(data, authors, cutoffs, num_feats):
+  vals = np.zeros(data.shape[1], len(set(authors)))
+  for feature in range(data.shape[1]):
+    for author in set(authors):
+      a = len(np.where(data[np.where(authors == author), feature] > cutoffs[feature])[0])
+      b = len(np.where(data[np.where(authors != author), feature] > cutoffs[feature])[0])
+      c = len(np.where(data[np.where(authors == author), feature] <= cutoffs[feature])[0])
+      d = len(np.where(data[np.where(authors != author), feature] <= cutoffs[feature])[0])
+      vals[feature, author] = float(a * d) / (c * b)
+  vals = np.amax(vals, axis=1)
+  return np.argpartition(vals, - num_feats)[-num_feats:]
